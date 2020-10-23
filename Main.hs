50c1bbe81bb1541f2c11f3fca9ce3b2887011d08
Tom Smeets <tom.tsmeets@gmail.com>
2020-10-23 12:44:10 +0200

    Play: Implement

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Gen
 import qualified Play
+import Gen (Size(Size), Pos(Pos), Dir(Dir), dir_add, pos_wrap)
 
 import qualified Data.Map as M
 import Data.Traversable
@@ -19,8 +20,27 @@ undupes [x] = [x]
 undupes (x1:x2:xs) | x1 == x2  = undupes (x1:xs)
                    | otherwise = x1:undupes (x2:xs)
 
+
+grid_points (Size w h) = Pos <$> [0..(w-1)] <*> [0..(h-1)]
+
+gen2play :: Gen.Grid -> Play.Grid
+gen2play g@(Gen.Grid size m) = Play.Grid size $ M.fromList $ map (\p -> (p, gen2play_tile p g)) (grid_points size)
+
+gen2play_tile :: Pos -> Gen.Grid -> Play.Tile
+gen2play_tile pos (Gen.Grid size m) = Play.mkTile $ zipWith (||) self around
+  where
+    self  = case M.lookup pos m of
+        Just d  -> map (== d)        Gen.dirs
+        Nothing -> map (const False) Gen.dirs
+    
+    around :: [Bool]
+    around = map (\p -> case M.lookup p m of
+                            Just d -> pos_wrap size (dir_add d p) == pos
+                            Nothing -> False
+                 ) $ map (`dir_add` pos) Gen.dirs
+
 main = do
     gen <- newStdGen
     let s    = Gen.Size 20 20
     let grid = undupes $ Gen.grid_gen s gen
-    simulate (InWindow "hsnet" (800, 600) (0, 0)) white 60 grid (scale 100 100 . Gen.grid_picture . head) (\_ _ g -> tailOrLast g)
+    simulate (InWindow "hsnet" (800, 600) (0, 0)) white 60 grid (scale 100 100 . Play.grid_picture . Play.shuffle . gen2play . head) (\_ _ g -> tailOrLast g)
--- Play.hs
+++ Play.hs
@@ -1,6 +1,36 @@
 module Play where
 
-data Tile = Tile Bool Bool Bool Bool deriving (Eq, Ord, Show)
+import qualified Data.Map as M
+import Data.Map (Map)
+import qualified Graphics.Gloss as G
+import Graphics.Gloss (Picture)
+import Gen (Dir(Dir), Pos(Pos), Size(Size), dirs)
 
+data Tile = Tile Bool Bool Bool Bool deriving (Eq, Ord, Show)
+data Grid = Grid Size (Map Pos Tile)
+
+mkTile :: [Bool] -> Tile
+mkTile [a, b, c, d] = Tile a b c d
+
+rot_left :: Tile -> Tile
 rot_left  (Tile a b c d) = Tile b c d a
+
+rot_right :: Tile -> Tile
 rot_right (Tile a b c d) = Tile d a b c
+
+tile_dirs :: Tile -> [Dir]
+tile_dirs (Tile a b c d) = map snd $ filter fst $ zip [a, b, c, d] dirs
+
+size_points_inside (Size w h) = Pos <$> [0..(w-1)] <*> [0..(h-1)]
+
+grid_picture :: Grid -> Picture
+grid_picture (Grid s m) = G.pictures $ map (\p@(Pos x y) -> G.translate (fromIntegral x) (fromIntegral y) $ tile_picture $ (m M.! p)) (size_points_inside s)
+
+tile_picture :: Tile -> Picture
+tile_picture t = G.pictures [ G.rectangleWire 1.0 1.0, G.color G.blue . G.pictures . map line . tile_dirs $ t ]
+  where
+    line (Dir x y) = G.line [(0, 0), (fromIntegral x / 2, fromIntegral y / 2)]
+
+
+grid_shuffle :: Grid -> Grid
+grid_shuffle (Grid s m) = Grid s $ M.map rot_left m
